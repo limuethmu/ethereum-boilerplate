eb091641e6ad607297077155a3786ab309199f92
limuethmu <160763235+limuethmu@users.noreply.github.com>
2024-02-21 10:45:50 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * Next.js Configuration
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  // Enables strict mode which enhances React's error handling.
   reactStrictMode: true,
+
+  // Enables SWC minification for faster build times.
   swcMinify: true,
+
+  // Image optimization configuration.
   images: {
+    // Domains from which images can be loaded.
     domains: ['raw.githubusercontent.com'],
+    // Additional image optimization options can be added here.
   },
+
+  // Add more configurations as needed.
 };
 
 module.exports = nextConfig;
